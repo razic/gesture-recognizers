afbf92311944f4007798a371ba70517cef1d9541
razic <razic@viralkitty.com>
2013-01-30 14:52:29 +0700

    gesture recognizer should set defaults

---
+++ spec/gesture-recognizer.js
@@ -0,0 +1,18 @@
+describe('GestureRecognizer', function() {
+  var gestureRecognizer;
+
+  beforeEach(function() {
+    gestureRecognizer = new GestureRecognizer();
+  });
+
+  it("should set some defaults", function() {
+    var state;
+    var touches;
+
+    state = gestureRecognizer.state;
+    touches = gestureRecognizer.touches;
+
+    expect(state).toBe('possible');
+    expect(touches).toEqual({});
+  });
+});
