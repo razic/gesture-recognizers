d584df7f830f90c45b3f412a64035dcad259f2d8
razic <razic@viralkitty.com>
2013-01-30 21:05:00 +0700

    add boiler plate spec for pan gesture recognizer

---
+++ spec/pan-gesture-recognizer.js
@@ -0,0 +1,9 @@
+describe('PanGestureRecognizer', function() {
+  'use strict';
+
+  var recognizer;
+
+  beforeEach(function() {
+    recognizer = new PanGestureRecognizer(document.body);
+  });
+});
