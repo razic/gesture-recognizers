60a1375a8774928f318f4e5c9bea2469e05f22ed
razic <razic@viralkitty.com>
2013-02-16 13:51:57 +0700

    make sure event listeners are set

--- spec/pan-gesture-recognizer.js
+++ spec/pan-gesture-recognizer.js
@@ -1,9 +1,21 @@
 describe('PanGestureRecognizer', function() {
   'use strict';
 
+  var target;
   var recognizer;
 
   beforeEach(function() {
-    recognizer = new PanGestureRecognizer(document.body);
+    target = document.body;
+  });
+
+  it('should set touch event listeners on the specified target', function(){
+    spyOn(target, 'addEventListener');
+
+    recognizer = new PanGestureRecognizer(target);
+
+    expect(target.addEventListener).
+    toHaveBeenCalledWith('touchstart', recognizer.touchStart, false);
+    expect(target.addEventListener).
+    toHaveBeenCalledWith('touchmove', recognizer.touchMove, false);
   });
 });
--- src/pan-gesture-recognizer.js
+++ src/pan-gesture-recognizer.js
@@ -1,21 +1,58 @@
 function PanGestureRecognizer(target) {
   'use strict';
 
-  this.minumumNumberOfTouches = 1;
+  this.startX = null;
+  this.startY = null;
+  this.translationX = null;
+  this.translationY = null;
+  this.minimumNumberOfTouches = 1;
   this.maximumNumberOfTouches = 10;
   this.minimumNumberOfPixelsTranslatedBeforeRecognized = 5;
 
-  this.touchStart = function(event) {
+  this.touchStart = (function(event) {
+    var lastTouch;
+
+    lastTouch = event.targetTouches[0];
+
+    this.startX = this.startX || lastTouch.clientX;
+    this.startY = this.startY || lastTouch.clientY;
+
+    event.preventDefault();
+  }).bind(this);
+
+  this.touchMove = (function(event) {
+    var lastTouch;
     var targetTouchesLength;
 
+    lastTouch = event.targetTouches[0];
     targetTouchesLength = event.targetTouches.length;
 
-    if (this.state == 'possible' && targetTouchesLength >= this.minimumNumberOfTouches && targetTouchesLength <= this.maximumNumberOfTouches) {
+    this.translationX = lastTouch.clientX - this.startX;
+    this.translationY = lastTouch.clientY - this.startY;
+
+    if (
+      this.state == 'possible' &&
+      targetTouchesLength >= this.minimumNumberOfTouches &&
+      targetTouchesLength <= this.maximumNumberOfTouches &&
+      this.totalPixelsTranslatedGreaterThanMinumum()
+    ) {
       this.state = 'began';
+    } else if (
+      this.state == 'began'
+    ) {
+      this.state = 'changed';
     }
+
+    event.preventDefault();
+  }).bind(this);
+
+  this.totalPixelsTranslatedGreaterThanMinumum = function(event) {
+    return this.translationX + this.translationY >=
+      this.minimumNumberOfPixelsTranslatedBeforeRecognized;
   };
 
   target.addEventListener('touchstart', this.touchStart, false);
+  target.addEventListener('touchmove', this.touchMove, false);
 }
 
 PanGestureRecognizer.prototype = new GestureRecognizer();
