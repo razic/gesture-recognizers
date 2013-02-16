23ec2cb954fc05326204af0c3e49a087f216e7fa
razic <razic@viralkitty.com>
2013-02-16 16:22:06 +0700

    pan gesture changes to the began state

--- spec/pan-gesture-recognizer.js
+++ spec/pan-gesture-recognizer.js
@@ -4,6 +4,17 @@ describe('PanGestureRecognizer', function() {
   var target;
   var recognizer;
 
+  function createTouchEvent(type, canBubble, cancelable, targetTouches) {
+    var event;
+
+    event = document.createEvent('UIEvents');
+    event.targetTouches = targetTouches;
+
+    event.initUIEvent(type, canBubble, cancelable);
+
+    return event;
+  }
+
   beforeEach(function() {
     target = document.body;
   });
@@ -17,5 +28,32 @@ describe('PanGestureRecognizer', function() {
     toHaveBeenCalledWith('touchstart', recognizer.touchStart, false);
     expect(target.addEventListener).
     toHaveBeenCalledWith('touchmove', recognizer.touchMove, false);
+  });
+
+  describe('when the minimum number of fingers allowed has moved enough to be \
+  considered a pan', function() {
+    beforeEach(function() {
+      var touchA;
+      var touchB;
+      var touchListA;
+      var touchListB;
+      var touchEventA;
+      var touchEventB;
+
+      recognizer = new PanGestureRecognizer(target);
+      touchA = { clientX: 10, clientY: 10 };
+      touchB = { clientX: 15, clientY: 10 };
+      touchListA = { 0: touchA, length: 1 };
+      touchListB = { 0: touchB, length: 1 };
+      touchEventA = createTouchEvent('touchstart', false, false, touchListA);
+      touchEventB = createTouchEvent('touchmove', false, false, touchListB);
+
+      target.dispatchEvent(touchEventA);
+      target.dispatchEvent(touchEventB);
+    });
+
+    it('changes to the began state', function() {
+      expect(recognizer.state).toBe('began');
+    });
   });
 });
