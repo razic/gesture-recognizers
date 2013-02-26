22a64ebc7e154c27280a0f54e91f8b6c720eb25b
razic <razic@viralkitty.com>
2013-02-26 22:46:18 +0700

    initialization error handling to the gesture recognizer abstract parent

--- spec/gesture-recognizer.js
+++ spec/gesture-recognizer.js
@@ -1,11 +1,89 @@
 describe('GestureRecognizer', function() {
-  var recognizer;
+  var abstractParent;
 
-  beforeEach(function() {
-    recognizer = new GestureRecognizer(document.body);
-  });
+  abstractParent = gestureRecognizers.GestureRecognizer;
 
-  it('should be in the possible state upon initialization', function() {
-    expect(recognizer.state).toBe('possible');
+  describe('initialization', function() {
+    describe('the number of arguments is not equal to two', function() {
+      it('throws an error', function() {
+        var errorMessage;
+
+        errorMessage = 'You must supply a target and action';
+
+        expect(abstractParent.initWithTarget).toThrow(errorMessage);
+      });
+    });
+
+    describe('the first argument is not an instance of Object', function() {
+      it('throws an error', function() {
+        var errorMessage;
+
+        errorMessage = '1 is not an instance of Object';
+
+        expect(function() {
+          abstractParent.initWithTarget(1, 2);
+        }).toThrow(errorMessage);
+      });
+    });
+
+    describe('the first argument is an instance of Object', function() {
+      describe('the second argument is not an instance of Object',
+      function() {
+        it('throws an error', function() {
+          var errorMessage;
+
+          errorMessage = '2 is not an instance of Object';
+
+          expect(function() {
+            abstractParent.initWithTarget({}, 2);
+          }).toThrow(errorMessage);
+        });
+      });
+
+      describe('the second argument is an instance of Object',
+      function() {
+        describe('the second argument does not have a property named \
+        `action`', function() {
+          it('throws an error', function() {
+            var errorMessage;
+
+            errorMessage = '[object Object] does not contain a property of';
+            errorMessage += ' name `action`';
+
+            expect(function() {
+              abstractParent.initWithTarget({}, {});
+            }).toThrow(errorMessage);
+          });
+        });
+
+        describe('the second argument does have a property named \
+        `action`', function() {
+          describe('the `action` property is not a function', function() {
+            it('throws an error', function() {
+              var errorMessage;
+
+              errorMessage = '[object Object]\'s `action` property is not a';
+              errorMessage += ' function';
+
+              expect(function() {
+                abstractParent.initWithTarget({}, { action: 1 });
+              }).toThrow(errorMessage);
+            });
+          });
+
+          describe('the `action` property is a function', function() {
+            var recognizer;
+
+            beforeEach(function() {
+              recognizer =
+                abstractParent.initWithTarget({}, { action: function() {} });
+            });
+
+            it('the recognizer will be in the `possible` state', function() {
+            });
+          });
+        });
+      });
+    });
   });
 });
--- src/gesture-recognizer.js
+++ src/gesture-recognizer.js
@@ -1,3 +1,21 @@
-function GestureRecognizer() {
-  this.state = 'possible';
-}
+function GestureRecognizer() {}
+
+GestureRecognizer.initWithTarget = function(target, options) {
+  if(arguments.length != 2) {
+    throw new Error('You must supply a target and action');
+  } else if(!(target instanceof Object)) {
+    throw new Error(target + ' is not an instance of Object');
+  } else if(!(options instanceof Object)) {
+    throw new Error(options + ' is not an instance of Object');
+  } else if(!('action' in options)) {
+    throw new Error(options + ' does not contain a property of name `action`');
+  } else if(!(options.action instanceof Function)) {
+    throw new Error(options + '\'s `action` property is not a function');
+  }
+
+};
+
+GestureRecognizer.prototype.state = 'possible';
+GestureRecognizer.prototype.targets = [];
+
+exports.GestureRecognizer = GestureRecognizer;
